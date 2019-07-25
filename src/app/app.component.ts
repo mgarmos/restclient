af7ae656a67a481d26234422e572c286d84bf1b5
Garcia Ramos <MAGARAMI@intranet.ibermatica>
2019-07-25 19:14:05 +0200

    List of posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  // Contiene los post que se van a recoger del servicio
+  posts = [];
+
   title = 'restClient';
+
+  constructor(private service: DataService) {
+    console.log('He entrado');
+    this.service.getData().subscribe(data =>  {
+      this.posts = data;
+    });
+    console.log('He salido');
+  }
+
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import {Post} from './post';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) {
+    console.log('DataService is working!');
+    this.getData();
+  }
+
+getData() {
+  return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
+}
+
+
+}
